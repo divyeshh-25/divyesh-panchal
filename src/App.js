55a491ab6306949ad54becbe51ce849f53f99158
divyeshh2508 <divyeshh2508@gmail.com>
2025-01-21 08:53:06 +0530

    SEO Friendly

--- src/App.js
+++ src/App.js
@@ -15,12 +15,50 @@ function App() {
   
   return (
     <div>
-      <Helmet>
-        <title>Home | My Website</title>
-        <meta name="description" content="Welcome to My Website, your go-to platform for amazing content." />
-        <meta name="keywords" content="React, SEO, React Helmet" />
-        <link rel="canonical" href="https://mywebsite.com/home" />
-      </Helmet>
+       <Helmet>
+
+      {/* Basic Meta Tags */}
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>Ram Maheshwari | RammCodes | Frontend Web Developer</title>
+
+      {/* Favicon */}
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+      {/* Meta Description */}
+      <meta
+        name="description"
+        content="A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product"
+      />
+
+      {/* Author */}
+      <meta name="author" content="Ram Maheshwari" />
+
+      {/* Google Site Verification */}
+      <meta
+        name="google-site-verification"
+        content="wX2xdkxqjrC4rA2EM-9bbaVpiEEFG8ae5azgeZDd-oo"
+      />
+
+      {/* Open Graph / Facebook Meta Tags */}
+      <meta
+        property="og:title"
+        content="Ram Maheshwari | RammCodes | Frontend Web Developer"
+      />
+      <meta property="og:site_name" content="Ram Maheshwari Portfolio Website" />
+      <meta property="og:url" content="https://www.rammaheshwari.com/" />
+      <meta
+        property="og:description"
+        content="A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product"
+      />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:image"
+        content="https://i.ibb.co/C8Wnkrt/pretty-snap-2022-3-29-16-11.png"
+      />
+    </Helmet>
       <Header />
       <Hero />
       <About />
